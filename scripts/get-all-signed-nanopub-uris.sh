16bd8d57e9e5e8799167bf813ebf89aa54a6c635
Tobias Kuhn <kuhntobias@gmail.com>
2023-07-19 17:31:38 +0200

    Update script

--- scripts/get-all-signed-nanopub-uris.sh
+++ scripts/get-all-signed-nanopub-uris.sh
@@ -6,7 +6,7 @@ cd ..
 mkdir -p load
 rm -f load/all-signed-nanopub-uris.txt
 
-for PAGE in {1..38}; do
+for PAGE in {1..50}; do
   echo "Downloading page $PAGE"
   curl -s -H "Accept: text/csv" "https://grlc.nps.petapico.org/api/local/local/find_signed_nanopubs?page=$PAGE" \
     | sed 1d \
