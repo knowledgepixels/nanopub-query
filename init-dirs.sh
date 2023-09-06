9468d3f98808980fdf9c6562ee13097806a1aa7b
Tobias Kuhn <kuhntobias@gmail.com>
2023-09-06 14:25:52 +0200

    Update init-dirs script

--- init-dirs.sh
+++ init-dirs.sh
@@ -5,5 +5,8 @@ cd "$(dirname "$0")"
 mkdir -p load
 mkdir -p data/rdf4j/data
 mkdir -p data/rdf4j/logs
+mkdir -p data/mongodb
+mkdir -p data/virtuoso
 
+sudo chmod 777 data/*
 sudo chmod 777 data/rdf4j/*
