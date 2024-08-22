746ab800eaf5f6832535a06c506bfc29df04195e
Tobias Kuhn <kuhntobias@gmail.com>
2024-08-22 15:15:44 +0200

    Create separate RDF4J proxy objects to see whether this fixes problem...

--- stresstest-local.sh
+++ stresstest-local.sh
@@ -2,7 +2,7 @@
 
 curl --silent 'http://localhost:9393/' \
   | grep -Po '/page/[a-zA-Z0-9/]+' \
-  | sed -r 's_^/page(.*)$_echo "\1"; curl --silent '\''http://localhost:9393/repo\1?query=select%20%2A%20where%20%7B%20graph%20%3Chttps%3A%2F%2Fw3id.org%2Fnp%2FRAdxdsL5vtExmiaydCI0yJCCoE5lkNksGr46KPEJUR37k%23assertion%3E%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D%20%7D'\''_' \
+  | sed -r 's_^/page(.*)$_echo "\1"; curl --silent '\''http://localhost:9393/repo\1?query=select%20%2A%20where%20%7B%20graph%20%3Chttps%3A%2F%2Fw3id.org%2Fnp%2FRAdxdsL5vtExmiaydCI0yJCCoE5lkNksGr46KPEJUR37k%23assertion%3E%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D%20%7D'\''; curl -X HEAD -v '\''http://localhost:9393/repo\1?query=select%20%2A%20where%20%7B%20graph%20%3Chttps%3A%2F%2Fw3id.org%2Fnp%2FRAdxdsL5vtExmiaydCI0yJCCoE5lkNksGr46KPEJUR37k%23assertion%3E%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D%20%7D'\''_' \
   | bash
 
 echo ""
