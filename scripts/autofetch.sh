c45328f03fa90f777bac9478bb9f498775feeb32
Tobias Kuhn <kuhntobias@gmail.com>
2024-12-02 08:36:04 +0100

    Fix autofetch problem

--- scripts/autofetch.sh
+++ scripts/autofetch.sh
@@ -13,6 +13,11 @@ for PAGE in $(seq 100); do
     | sed 1d | awk -F, '{print $1}' | sed 's/"//g' \
     >> nanopubs-autofetch.temp.txt
 done
-mv nanopubs-autofetch.temp.txt nanopubs-autofetch.txt
+
+# Sometimes pubkey (wrongly) have newlines in them, and then the above script returns
+# extra lines that we exclude here:
+cat nanopubs-autofetch.temp.txt | grep -E '^https?://' > nanopubs-autofetch.temp2.txt
+rm nanopubs-autofetch.temp.txt
+mv nanopubs-autofetch.temp2.txt nanopubs-autofetch.txt
 
 echo "Autofetch done"
