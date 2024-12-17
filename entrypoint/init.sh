4b5e1933e2c4079912078d1639a9d2045f6df739
Tobias Kuhn <kuhntobias@gmail.com>
2024-12-17 08:51:14 +0100

    Fix typo in comment

--- entrypoint/init.sh
+++ entrypoint/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is needed to the main Java command doesn't get PID 1 and therefore
+# This script is needed so the main Java command doesn't get PID 1 and therefore
 # it can be killed on a negative healthcheck.
 
 catalina.sh run
