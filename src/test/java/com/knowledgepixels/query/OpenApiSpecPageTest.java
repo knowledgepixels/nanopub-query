e844ec2edfa363346faa0b56e8f122035f08514f
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-13 13:23:13 +0200

    test: add unit tests for OpenApiSpecPage

---
+++ src/test/java/com/knowledgepixels/query/OpenApiSpecPageTest.java
@@ -0,0 +1,81 @@
+package com.knowledgepixels.query;
+
+import io.vertx.core.MultiMap;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.nanopub.MalformedNanopubException;
+import org.nanopub.Nanopub;
+import org.nanopub.NanopubImpl;
+import org.nanopub.extra.server.GetNanopub;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+
+class OpenApiSpecPageTest {
+
+    private final String baseUri = "https://w3id.org/np/";
+    private final String artifactCode = "RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA";
+    private final String queryName = "get-participation";
+
+    @Test
+    void constructWithInvalidUrl() {
+        OpenApiSpecPage page = new OpenApiSpecPage("https://invalid-url", MultiMap.caseInsensitiveMultiMap());
+        assertNull(page.getSpec());
+    }
+
+    @Test
+    void constructWithParameters() throws MalformedNanopubException, IOException {
+        try (MockedStatic<GetNanopub> mockedGetNanopub = mockStatic(GetNanopub.class)) {
+            Nanopub mockNanopub = new NanopubImpl(new File(Objects.requireNonNull(this.getClass().getResource("/grlc-query.trig")).getPath()));
+            mockedGetNanopub.when(() -> GetNanopub.get(any())).thenReturn(mockNanopub);
+            MultiMap parameters = MultiMap.caseInsensitiveMultiMap();
+            parameters.add("api-version", "latest");
+            OpenApiSpecPage page = new OpenApiSpecPage(baseUri + artifactCode + "/" + queryName, parameters);
+            assertNotNull(page);
+        }
+    }
+
+    @Test
+    void getSpec() throws MalformedNanopubException, IOException {
+        try (MockedStatic<GetNanopub> mockedGetNanopub = mockStatic(GetNanopub.class)) {
+            Nanopub mockNanopub = new NanopubImpl(new File(Objects.requireNonNull(this.getClass().getResource("/grlc-query.trig")).getPath()));
+            mockedGetNanopub.when(() -> GetNanopub.get(any())).thenReturn(mockNanopub);
+            MultiMap parameters = MultiMap.caseInsensitiveMultiMap();
+            OpenApiSpecPage page = new OpenApiSpecPage(baseUri + artifactCode + "/" + queryName, parameters);
+            String expectedSpec = """
+                    openapi: 3.0.4
+                    info:
+                      title: Get participation links
+                      description: 'API definition source: <a target="_blank" href="https://w3id.org/np/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA"><svg
+                        height="0.8em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path d="M5,8H8L3,0H0M8,4.8V0H5M0,3.2V8H3"/></svg>
+                        RA6T-YLqLn</a>'
+                      version: RA6T-YLqLn
+                    servers:
+                    - url: http://localhost:9393/api/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA
+                      description: This Nanopub Query instance
+                    paths:
+                      /get-participation:
+                        get:
+                          description: This query returns all participation links.
+                          parameters: [
+                            ]
+                          responses:
+                            '200':
+                              content:
+                                text/csv: {
+                                  }
+                                application/json: {
+                                  }
+                              description: result table
+                    """;
+
+            assertEquals(expectedSpec, page.getSpec());
+        }
+    }
+
+}
