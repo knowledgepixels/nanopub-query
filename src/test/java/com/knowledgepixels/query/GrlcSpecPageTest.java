1d0c1e912f7df559464c677d4ecdb3d80fbf8817
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-19 09:10:42 +0200

    test(GrlcSpecPage): fix line separator in expected strings

--- src/test/java/com/knowledgepixels/query/GrlcSpecPageTest.java
+++ src/test/java/com/knowledgepixels/query/GrlcSpecPageTest.java
@@ -71,24 +71,24 @@ class GrlcSpecPageTest {
                     #+ licence: http://www.apache.org/licenses/LICENSE-2.0
                     #+ endpoint: http://query:9393/repo/full
                     
-                    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                    prefix dct: <http://purl.org/dc/terms/>
-                    prefix np: <http://www.nanopub.org/nschema#>
-                    prefix npa: <http://purl.org/nanopub/admin/>
-                    prefix npx: <http://purl.org/nanopub/x/>
-                    prefix wd: <http://www.wikidata.org/entity/>
-                    
-                    select ?person ?event ?np ?date where {
-                      graph npa:graph {
-                        ?np npa:hasValidSignatureForPublicKey ?pubkey .
-                        filter not exists { ?npx npx:invalidates ?np ; npa:hasValidSignatureForPublicKey ?pubkey . }
-                        ?np dct:created ?date .
-                        ?np np:hasAssertion ?a .
-                        optional { ?np rdfs:label ?label }
-                      }
-                      graph ?a {
-                        ?person wd:P1344 ?event .
-                      }
+                    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r
+                    prefix dct: <http://purl.org/dc/terms/>\r
+                    prefix np: <http://www.nanopub.org/nschema#>\r
+                    prefix npa: <http://purl.org/nanopub/admin/>\r
+                    prefix npx: <http://purl.org/nanopub/x/>\r
+                    prefix wd: <http://www.wikidata.org/entity/>\r
+                    \r
+                    select ?person ?event ?np ?date where {\r
+                      graph npa:graph {\r
+                        ?np npa:hasValidSignatureForPublicKey ?pubkey .\r
+                        filter not exists { ?npx npx:invalidates ?np ; npa:hasValidSignatureForPublicKey ?pubkey . }\r
+                        ?np dct:created ?date .\r
+                        ?np np:hasAssertion ?a .\r
+                        optional { ?np rdfs:label ?label }\r
+                      }\r
+                      graph ?a {\r
+                        ?person wd:P1344 ?event .\r
+                      }\r
                     } order by desc(?date)""";
             assertEquals(expectedSpec, page.getSpec());
         }
