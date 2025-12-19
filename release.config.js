4ceb494c9c13e0d3e2f3479b1fbc61fbcd1ffe9a
Ashley Caselli <ashley@knowledgepixels.com>
2025-12-19 12:01:12 +0100

    chore(release): update config to use a different mavenTarget

--- release.config.js
+++ release.config.js
@@ -1,7 +1,3 @@
-let publishCmd = `
-docker build -t "$IMAGE_NAME:\${nextRelease.version}" -t "$IMAGE_NAME:latest" .
-docker push --all-tags "$IMAGE_NAME"
-`
 let config = require('semantic-release-preconfigured-conventional-commits');
 config.tagFormat = 'nanopub-query-${version}'
 config.branches = ['release']
@@ -9,15 +5,10 @@ config.plugins.push(
   [
     "@terrestris/maven-semantic-release",
     {
+      "mavenTarget": "package jib:build",
       "settingsPath": "./settings.xml",
       "updateSnapshotVersion": true,
       "mvnw": true
-    }
-  ],
-  [
-    "@semantic-release/exec",
-    {
-      "publishCmd": publishCmd
     }
   ],
   "@semantic-release/github",
