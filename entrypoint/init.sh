0a10d04afc6a99233d7353b1416d762e50dc88d7
Tobias Kuhn <kuhntobias@gmail.com>
2026-01-08 15:27:49 +0100

    fix: Revert and switch to other rdf4j image, to solve curl problem

--- entrypoint/init.sh
+++ entrypoint/init.sh
@@ -3,9 +3,4 @@
 # This script is needed so the main Java command doesn't get PID 1 and therefore
 # it can be killed on a negative healthcheck.
 
-# Install curl if not already installed (needed for healthcheck)
-if ! command -v curl &> /dev/null; then
-    apt-get update && apt-get install -y curl && rm -rf /var/lib/apt/lists/*
-fi
-
 catalina.sh run
